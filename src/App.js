2a4be9c99e7aca6c7292632badfd38f1042e6db2
sumitjambharkar <sujam3545@gmail.com>
2023-11-21 13:05:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Profile from "./components/userRoutes/Profile";
+import User from "./components/userRoutes/User";
+import Home from "./components/Home";
+import Admin from "./components/adminRoutes/User"
 
-function App() {
+const USER_TYPES = {
+  PUBLIC: "Public User",
+  NORMAL_USER: "Normal User",
+  ADMIN_USER: "Admin User",
+};
+const CURRENT_USER_TYPE = USER_TYPES.NORMAL_USER;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<PublicElements><Home /></PublicElements>}/>
+        <Route path="/user" element={<UserElements><User /></UserElements>}/>
+        <Route path="/profile"element={<UserElements><Profile /></UserElements>}/>
+        <Route path="/admin" element={<AdminElements><Admin /></AdminElements>}/>
+        <Route path="*" element={<div>Page Not Found!</div>} />
+      </Routes>
+    </Router>
   );
+};
+
+
+function PublicElements({ children }) {
+  return <>{children}</>;
+}
+
+function UserElements({ children }) {
+  if (
+    CURRENT_USER_TYPE === USER_TYPES.NORMAL_USER ||
+    CURRENT_USER_TYPE === USER_TYPES.ADMIN_USER
+  ) {
+    return <>{children}</>;
+  } else {
+    return <Navigate to="/" />;
+  }
+}
+
+function AdminElements({ children }) {
+  if (CURRENT_USER_TYPE === USER_TYPES.ADMIN_USER) {
+    return <>{children}</>;
+  } else {
+    return <Navigate to="/" />;
+  }
 }
 
 export default App;
